4e1f0d0c8b373bf8cbf21037197143d62dc9ec30
Emmyjosh65 <ge5853987@gmail.com>
2026-05-29 19:31:59 +0100

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,17 @@
+function sendMessage() {
+    const input = document.getElementById("messageInput");
+    const messages = document.getElementById("messages");
 
+    if(input.value.trim() !== ""){
+
+        const msg = document.createElement("div");
+        msg.classList.add("message");
+        msg.innerText = input.value;
+
+        messages.appendChild(msg);
+
+        input.value = "";
+
+        messages.scrollTop = messages.scrollHeight;
+    }
+}

c3ad2ca3d4ce4e766c6706f53ab063650771c54f
Emmyjosh65 <ge5853987@gmail.com>
2026-05-29 19:37:29 +0100

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -15,3 +15,84 @@ function sendMessage() {
         messages.scrollTop = messages.scrollHeight;
     }
 }
+// LOAD USERS
+function loadUsers(){
+
+    const users = [
+        {
+            name:"Daniel",
+            number:"+2349011111111"
+        },
+
+        {
+            name:"Jessica",
+            number:"+2349022222222"
+        },
+
+        {
+            name:"Michael",
+            number:"+2349033333333"
+        }
+    ];
+
+    const usersList = document.getElementById("usersList");
+
+    users.forEach(user => {
+
+        const div = document.createElement("div");
+
+        div.classList.add("user");
+
+        div.innerHTML = `
+            <strong>Name:</strong> ${user.name}<br>
+            <strong>Number:</strong> ${user.number}
+        `;
+
+        usersList.appendChild(div);
+
+    });
+
+}
+
+
+// LOAD COMPLAINTS
+function loadComplaints(){
+
+    const complaints = [
+
+        {
+            user:"Daniel",
+            message:"Messages not sending"
+        },
+
+        {
+            user:"Jessica",
+            message:"Account login problem"
+        },
+
+        {
+            user:"Michael",
+            message:"Profile picture not uploading"
+        }
+
+    ];
+
+    const complaintsList =
+    document.getElementById("complaintsList");
+
+    complaints.forEach(c => {
+
+        const div = document.createElement("div");
+
+        div.classList.add("complaint");
+
+        div.innerHTML = `
+            <strong>User:</strong> ${c.user}<br>
+            <strong>Complaint:</strong> ${c.message}
+        `;
+
+        complaintsList.appendChild(div);
+
+    });
+
+}

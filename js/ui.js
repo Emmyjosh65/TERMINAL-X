fd9e82507404aa90dc8de82ddabade4d2e529b05
Emmyjosh65 <ge5853987@gmail.com>
2026-05-29 19:32:28 +0100

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,9 @@
+// Dark mode toggle example
+function toggleTheme(){
+    document.body.classList.toggle("dark");
+}
 
+// Welcome animation
+window.onload = () => {
+    console.log("TERMINAL X Loaded");
+};

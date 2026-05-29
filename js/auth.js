78442da8134663db74884bf23269bc2619f09d3e
Emmyjosh65 <ge5853987@gmail.com>
2026-05-29 19:31:29 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,36 @@
+// SIGN UP
+function signup() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(() => {
+        alert("Signup Successful");
+        window.location.href = "chat.html";
+    })
+    .catch(error => {
+        alert(error.message);
+    });
+}
+
+// LOGIN
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+        alert("Login Successful");
+        window.location.href = "chat.html";
+    })
+    .catch(error => {
+        alert(error.message);
+    });
+}
+
+// LOGOUT
+function logout() {
+    auth.signOut().then(() => {
+        window.location.href = "login.html";
+    });
+}
